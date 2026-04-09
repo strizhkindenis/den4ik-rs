5fc971cfe3ab77af26dc3b82f1d12f82b439aaa6
Denis Strizhkin <strdenis02@gmail.com>
2026-04-09 20:16:48 +0300

    den4ik_math: introduce VecN

--- crates/den4ik_math/src/lib.rs
+++ crates/den4ik_math/src/lib.rs
@@ -11,7 +11,9 @@
 mod float;
 pub mod mat;
 pub mod traits;
+pub mod vec_n;
 
 pub use float::{Float, FloatConsts};
 pub use mat::{Dims, Mat};
 pub use traits::{One, Zero};
+pub use vec_n::{Vec1, Vec2, Vec3, Vec4};
