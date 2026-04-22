793806496eff486c48dd23d87d000c0fcf5d9cd9
Denis Strizhkin <strdenis02@gmail.com>
2026-04-23 01:32:59 +0300

    den4ik_math: remove bs

---
+++ crates/den4ik_math/src/f32.rs
@@ -0,0 +1,251 @@
+use std::{
+	alloc::{alloc, dealloc, handle_alloc_error, Layout},
+	ptr::NonNull,
+	iter::{self, IntoIterator},
+	slice,
+};
+
+
+fn layoutnf32(n: usize) -> Layout {
+	Layout::array::<f32>(n).unwrap()
+}
+
+fn allocnf32(n: usize) -> NonNull<f32> {
+	let layout = layoutnf32(n);
+	let p = unsafe { alloc(layout) };
+	if p.is_null() {
+		handle_alloc_error(layout);
+	}
+	NonNull::new(p.cast()).unwrap()
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Index(pub [usize; 2]);
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Dims(pub [usize; 2]);
+
+impl Dims {
+	pub fn size(self) -> usize {
+		self.0[0] * self.0[1]
+	}
+	
+	pub fn get_idx(self, idx: Index) -> usize {
+		assert!(idx.0[0] < self.0[0]);
+		assert!(idx.0[1] < self.0[1]);
+		idx.0[0] * self.0[1] + idx.0[1]	
+	}
+	
+	pub fn t(self) -> Self {
+		Self([self.0[1], self.0[0]])	
+	}
+}
+
+pub struct MatF32 {
+	p: NonNull<f32>,
+	dims: Dims,
+}
+
+impl Drop for MatF32 {
+	fn drop(&mut self) {
+		let layout = layoutnf32(self.len());
+		let p = self.as_ptr_mut().cast();
+		unsafe { dealloc(p, layout) };	
+	}
+}
+
+impl MatF32 {
+	pub unsafe fn unew(dims: Dims) -> Self {
+		let p = allocnf32(dims.size());
+		Self { p, dims }
+	}
+	
+	pub fn apply_fn<F>(&mut self, mut f: F)
+	where F: FnMut(&mut f32) 
+	{
+		self.apply_fn_idx(|x, _| f(x));	
+	}
+	
+	pub fn apply_fn_idx<F>(&mut self, mut f: F)
+	where F: FnMut(&mut f32, Index)
+	{
+		for i0 in 0..self.dims.0[0] {
+			for i1 in 0..self.dims.0[1] {
+				let idx = Index([i0, i1]);
+				f(self.at_mut(idx), idx);
+			}
+		}
+	}
+	
+	pub fn from_esiter<I>(dims: Dims, iter: I) -> Self
+	where
+		I: IntoIterator<Item = f32>, 
+		I::IntoIter: ExactSizeIterator 
+	{
+		let iter = iter.into_iter();
+		assert_eq!(iter.len(), dims.size());
+		let mut m = unsafe { Self::unew(dims) };
+		for (a, b) in iter::zip(m.iter_mut(), iter) {
+			*a = b;	
+		}
+		m
+	}
+	
+	pub fn from_fn<F>(dims: Dims, mut f: F) -> Self
+	where F: FnMut() -> f32
+	{
+		Self::from_fn_idx(dims, |_| f())	
+	}
+	
+	pub fn from_fn_idx<F>(dims: Dims, mut f: F) -> Self
+	where F: FnMut(Index) -> f32
+	{
+		let mut m = unsafe { Self::unew(dims) };		
+		m.apply_fn_idx(|x, idx| *x = f(idx));
+		m
+	}
+	
+	pub fn from_x(dims: Dims, x: f32) -> Self {
+		Self::from_esiter(dims, iter::repeat_n(x, dims.size()))	
+	}
+	
+	pub fn as_ptr(&self) -> *const f32 {
+		self.p.as_ptr().cast()	
+	}
+	
+	pub fn as_ptr_mut(&mut self) -> *mut f32 {
+		self.p.as_ptr().cast()	
+	}
+	
+	pub fn as_slice(&self) -> &[f32] {
+		unsafe { slice::from_raw_parts(self.as_ptr(), self.len()) }
+	}
+	
+	pub fn as_slice_mut(&mut self) -> &mut [f32] {
+		unsafe { slice::from_raw_parts_mut(self.as_ptr_mut(), self.len()) }
+	}
+	
+	pub fn iter(&self) -> slice::Iter<'_, f32> {
+		self.as_slice().iter()	
+	}
+	
+	pub fn iter_mut(&mut self) -> slice::IterMut<'_, f32> {
+		self.as_slice_mut().iter_mut()	
+	}
+	
+	pub fn at(&self, idx: Index) -> &f32 {
+		let i = self.dims.get_idx(idx);
+		&self.as_slice()[i]	
+	}
+	
+	pub fn at_mut(&mut self, idx: Index) -> &mut f32 {
+		let i = self.dims.get_idx(idx);
+		&mut self.as_slice_mut()[i]	
+	}
+	
+	pub fn opp<F>(a: &Self, b: &Self, c: &mut Self, mut f: F)
+	where F: FnMut(&f32, &f32, &mut f32)
+	{
+		assert_eq!(a.dims, b.dims);
+		assert_eq!(a.len(), c.len());
+		c.dims = a.dims;
+		let iter = iter::zip(a.iter(), b.iter());
+		for ((a, b), c) in iter::zip(iter, c.iter_mut()) {
+			f(a, b, c);	
+		}
+	}
+	
+	/// ```
+	/// use den4ik_math::f32::{MatF32, Dims};
+	/// let dims = Dims([2, 3]);
+	/// let a = MatF32::from_esiter(dims, [
+	///     1.0, 3.0, 2.0,
+	///     4.0, 0.0, 1.0,
+	/// ]);
+	/// let b = MatF32::from_esiter(dims, [
+	///     5.0, 2.0,  1.0,
+	///     0.0, 3.0, -2.0,
+	/// ]);
+	/// let mut mc = MatF32::from_x(dims, 0.0);
+	/// let c = MatF32::from_esiter(dims, [
+	///     6.0, 5.0,  3.0,
+	///     4.0, 3.0, -1.0,
+	/// ]);
+	/// MatF32::add(&a, &b, &mut mc);
+	/// MatF32::assert_eq_eps(&mc, &c, 1e-6);
+	/// ```
+	pub fn add(a: &Self, b: &Self, c: &mut Self) {
+		Self::opp(a, b, c, |a, b, c| *c = a + b);	
+	}
+	
+	/// ```
+	/// use den4ik_math::f32::{MatF32, Dims};
+	/// let dims = Dims([2, 3]);
+	/// let a = MatF32::from_esiter(dims, [
+	///     1.0, 3.0, 2.0,
+	///     4.0, 0.0, 1.0,
+	/// ]);
+	/// let b = MatF32::from_esiter(dims, [
+	///     5.0, 2.0,  1.0,
+	///     0.0, 3.0, -2.0,
+	/// ]);
+	/// let mut mc = MatF32::from_x(dims, 0.0);
+	/// let c = MatF32::from_esiter(dims, [
+	///     -4.0, 1.0,  1.0,
+	///      4.0, -3.0, 3.0,
+	/// ]);
+	/// MatF32::sub(&a, &b, &mut mc);
+	/// MatF32::assert_eq_eps(&mc, &c, 1e-6);
+	/// ```
+	pub fn sub(a: &Self, b: &Self, c: &mut Self) {
+		Self::opp(a, b, c, |a, b, c| *c = a - b);	
+	}
+	
+	pub fn mul(a: &Self, b: &Self, c: &mut Self) {
+		Self::opp(a, b, c, |a, b, c| *c = a * b);	
+	}
+	
+	pub fn div(a: &Self, b: &Self, c: &mut Self) {
+		Self::opp(a, b, c, |a, b, c| *c = a / b);	
+	}
+	
+	pub fn t(&self, t: &mut Self) {
+		assert_eq!(self.len(), t.len());
+		t.dims = self.dims.t();
+		t.apply_fn_idx(|t, idx| *t = *self.at(Index([idx.0[1], idx.0[0]])));	
+	}
+	
+	pub fn dot(a: &Self, b: &Self, c: &mut Self) {
+		assert_eq!(a.dims.0[1], b.dims.0[0]);
+		let dims = Dims([a.dims.0[0], b.dims.0[1]]);
+		assert_eq!(dims.size(), c.len());
+		let mut bt = unsafe { Self::unew(b.dims) };
+		b.t(&mut bt);
+		c.dims = dims;
+		c.apply_fn_idx(|c, idx| {
+			let a_start = idx.0[0] * a.dims.0[1];
+			let a_end = a_start + a.dims.0[1];
+			let bt_start = idx.0[1] * bt.dims.0[1];
+			let bt_end = bt_start + bt.dims.0[1];
+			let sa = &a.as_slice()[a_start..a_end];
+			let sbt = &bt.as_slice()[bt_start..bt_end];
+			*c = sa.iter().zip(sbt.iter()).map(|(a, bt)| a * bt).sum();	
+		});
+	}
+	
+	pub fn len(&self) -> usize {
+		self.dims.size()
+	}
+	
+	pub fn assert_eq_eps(a: &Self, b: &Self, eps: f32) {
+		assert!(eps > 0.0, "{eps} <= 0");
+		assert_eq!(a.dims, b.dims, "a.dims({:?}) != b.dims({:?})", a.dims, b.dims);
+		Self::from_fn_idx(a.dims, |idx| {
+			let a = a.at(idx);
+			let b = b.at(idx);
+			let err = (a - b).abs();
+			assert!(err < eps, "at {idx:?}: {a} != {b}");  
+			err
+		});
+	}
+}
--- crates/den4ik_math/src/lib.rs
+++ crates/den4ik_math/src/lib.rs
@@ -8,10 +8,4 @@
     clippy::pedantic
 )]
 
-pub mod mat;
-pub mod traits;
-pub mod vec_n;
-
-pub use mat::{Dims, Mat};
-pub use traits::{Num, Float};
-pub use vec_n::{Vec1, Vec2, Vec3, Vec4};
+pub mod f32;
